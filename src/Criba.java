eb1f45ea0abcad9f364a3b12d3a5a0065bee7801
Bianca Milea <alumil2013@ieselcaminas.org>
2023-03-05 20:27:33 +0100

    Refactorización código

--- src/Criba.java
+++ src/Criba.java
@@ -1,40 +1,43 @@
 public class Criba {
-    // Generar números primos de 1 a max
-    public static int[] generarPrimos(int max) {
-        int i, j;
-        if (max >= 2) {
-            // Declaraciones
-            int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
-            // Inicializar el array
-            for (i = 0; i < dim; i++)
-                esPrimo[i] = true;
-            // Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
-            // Criba
-            for (i = 2; i < Math.sqrt(dim) + 1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j = 2 * i; j < dim; j += i)
-                        esPrimo[j] = false;
+    // Generate prime numbers from 1 up to max
+    public static int[] generatePrimes(int max) {
+        if (max < 2) {
+            return new int[0];
+            // Empty array for max < 2
+        }
+
+        // Initialize array
+        int size = max + 1;
+        boolean[] isPrime = new boolean[size];
+        for (int i = 0; i < size; i++) {
+            isPrime[i] = true;
+        }
+
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i <= max; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= max; j += i) {
+                    isPrime[j] = false;
                 }
             }
-            // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
-            // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i = 0, j = 0; i < dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
-            return primos;
-        } else { // max < 2
-            return new int[0];
-            // Vector vacío
         }
+
+        // Count number of primes
+        int count = 0;
+        for (int i = 2; i <= max; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        // Fill prime numbers into array
+        int[] primes = new int[count];
+        for (int i = 2, j = 0; i <= max; i++) {
+            if (isPrime[i]) {
+                primes[j++] = i;
+            }
+        }
+
+        return primes;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato=teclado.nextInt();
-        int vector[]=new int[dato];
-        System.out.println("\nVector inicial hasta :"+dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(i+1+"\t");
+        int maxNumber = scanner.nextInt();
+        int[] primes = Criba.generatePrimes(maxNumber);
+
+        System.out.println("\nVector inicial hasta " + maxNumber + ":");
+        printNumbersUpTo(maxNumber);
+
+        System.out.println("\nVector de primos hasta " + maxNumber + ":");
+        printNumbers(primes);
+    }
+
+    private static void printNumbersUpTo(int maxNumber) {
+        for (int i = 0; i < maxNumber; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.print((i + 1) + "\t");
         }
-        vector= Criba.generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(vector[i]+"\t");
+    }
+
+    private static void printNumbers(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            System.out.print(numbers[i] + "\t");
         }
     }
 }
