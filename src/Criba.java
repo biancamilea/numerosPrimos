1d0f37824677730ee458dba0b023616150fa5401
Bianca Milea <alumil2013@ieselcaminas.org>
2023-03-05 20:40:04 +0100

    Documentación de código con JavaDoc

--- src/Criba.java
+++ src/Criba.java
@@ -1,43 +1,64 @@
+/**
+
+ La clase Criba contiene el método estático generarPrimos, el cual devuelve un array de números primos
+
+ hasta un número máximo dado.
+ */
 public class Criba {
-    // Generate prime numbers from 1 up to max
-    public static int[] generatePrimes(int max) {
-        if (max < 2) {
-            return new int[0];
-            // Empty array for max < 2
-        }
 
-        // Initialize array
-        int size = max + 1;
-        boolean[] isPrime = new boolean[size];
-        for (int i = 0; i < size; i++) {
-            isPrime[i] = true;
-        }
+    /**
 
-        // Sieve of Eratosthenes
-        for (int i = 2; i * i <= max; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= max; j += i) {
-                    isPrime[j] = false;
+     Genera un array con los números primos hasta el número máximo dado.
+
+     @param max el número máximo hasta el cual se generarán los números primos.
+
+     @return un array de números primos.
+     */
+    public static int[] generarPrimos(int max) {
+        int i, j;
+
+        // Se comprueba que el número máximo es mayor o igual a 2.
+        if (max >= 2) {
+            // Se declara un array de booleanos para marcar qué números son primos.
+            int dim = max + 1; // Tamaño del array
+            boolean[] esPrimo = new boolean[dim];
+            // Se inicializa el array con todos los elementos a true.
+            for (i = 0; i < dim; i++) {
+                esPrimo[i] = true;
+            }
+
+            // Se marcan el 0 y el 1 como no primos.
+            esPrimo[0] = esPrimo[1] = false;
+
+            // Se utiliza la criba de Eratóstenes para marcar los números no primos.
+            for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+                if (esPrimo[i]) {
+                    // Se eliminan los múltiplos de i marcándolos como no primos.
+                    for (j = 2 * i; j < dim; j += i) {
+                        esPrimo[j] = false;
+                    }
                 }
             }
-        }
 
-        // Count number of primes
-        int count = 0;
-        for (int i = 2; i <= max; i++) {
-            if (isPrime[i]) {
-                count++;
+            // Se cuenta el número de números primos encontrados.
+            int cuenta = 0;
+            for (i = 0; i < dim; i++) {
+                if (esPrimo[i]) {
+                    cuenta++;
+                }
             }
-        }
 
-        // Fill prime numbers into array
-        int[] primes = new int[count];
-        for (int i = 2, j = 0; i <= max; i++) {
-            if (isPrime[i]) {
-                primes[j++] = i;
+            // Se rellena el array de números primos.
+            int[] primos = new int[cuenta];
+            for (i = 0, j = 0; i < dim; i++) {
+                if (esPrimo[i]) {
+                    primos[j++] = i;
+                }
             }
+            return primos;
+        } else { // max < 2
+            // Si el número máximo es menor que 2 se devuelve un array vacío.
+            return new int[0];
         }
-
-        return primes;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,40 @@
+/**
+
+ La clase Main es la clase principal del programa que utiliza la clase Criba para
+ generar números primos.
+ */
 import java.util.Scanner;
-
 public class Main {
+
+    /**
+     * El método main recibe un número máximo desde el usuario y muestra por consola
+     * un vector con los números primos generados por la clase Criba.
+     *
+     * @param args los argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce el número para la criba de Erastótenes:");
-        int maxNumber = scanner.nextInt();
-        int[] primes = Criba.generatePrimes(maxNumber);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el número máximo para la criba de Eratóstenes:");
+        int dato = teclado.nextInt();
 
-        System.out.println("\nVector inicial hasta " + maxNumber + ":");
-        printNumbersUpTo(maxNumber);
-
-        System.out.println("\nVector de primos hasta " + maxNumber + ":");
-        printNumbers(primes);
-    }
-
-    private static void printNumbersUpTo(int maxNumber) {
-        for (int i = 0; i < maxNumber; i++) {
+        // Mostrar vector inicial
+        int vector[] = new int[dato];
+        System.out.println("\nVector inicial hasta: " + dato);
+        for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) {
                 System.out.println();
             }
-            System.out.print((i + 1) + "\t");
+            System.out.print(i + 1 + "\t");
         }
-    }
 
-    private static void printNumbers(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
+        // Generar vector de primos y mostrarlo
+        vector = Criba.generarPrimos(dato);
+        System.out.println("\nVector de primos hasta: " + dato);
+        for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) {
                 System.out.println();
             }
-            System.out.print(numbers[i] + "\t");
+            System.out.print(vector[i] + "\t");
         }
     }
 }
